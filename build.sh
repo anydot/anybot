8ab398908c578f9329df9298143be66e63b16feb
Premysl Hruby <dfenze@gmail.com>
2020-08-13 10:11:25 +0200

    build: backup also the config

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 set -eu
 cd `dirname $0`
 
-test -e ../app && mv -f ../app ../app-$(date --iso-8601=minutes)
+bkp_folder=../bkp/$(date --iso-8601=minutes)/
+mkdir -p $bkp_folder
+
+test -e ../app && mv -f ../app $bkp_folder
+test -e ../appsettings.json && cp ../appsettings.json $bkp_folder
 
 dotnet build
 dotnet test
