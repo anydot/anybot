91448cec41bec6144dac5a8cfb52c2471d64188e
anydot <dfenze@gmail.com>
2024-01-07 12:09:25 +0100

    Fixes issue with deleting the file we just wrote to the storage (#540)

--- RoumenBot/RoumenResponseLogger.cs
+++ RoumenBot/RoumenResponseLogger.cs
@@ -7,6 +7,7 @@ namespace RoumenBot
 {
     public sealed class RoumenResponseLogger<T> : IRoumenResponseLogger<T> where T : ITag, new()
     {
+        private const string dateFormatString = "yyyy-MM-dd-HH-mm-ss";
         readonly TimeSpan MaxAge = TimeSpan.FromDays(7);
 
         private readonly IStorage<string> storage;
@@ -18,13 +19,13 @@ namespace RoumenBot
 
         public void LogResponse(string response)
         {
-            var datetime = DateTimeOffset.UtcNow.ToString("yyyy-MM-dd HH:mm:ss");
+            var datetime = DateTimeOffset.UtcNow.ToString(dateFormatString);
 
             storage.Write(datetime, response);
 
             foreach (var key in storage.Iterate().Select(kvp => kvp.Key))
             {
-                if (DateTimeOffset.TryParse(key, CultureInfo.InvariantCulture, out var parsed) || parsed < DateTimeOffset.UtcNow - MaxAge)
+                if (!DateTimeOffset.TryParseExact(key!, dateFormatString, CultureInfo.InvariantCulture, DateTimeStyles.None, out var parsed) || parsed < DateTimeOffset.UtcNow - MaxAge)
                 {
                     storage.Delete(key);
                 }
